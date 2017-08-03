2df860bb327af95335644120233116ae877039d7
jljianglin <861711433@qq.com>
2017-08-03 18:15:33 +0800

    Initial commit

---
+++ src/cn/bdqn/tcmp056/test/Test.java
@@ -0,0 +1,11 @@
+package cn.bdqn.tcmp056.test;
+
+/**
+ * Created by 刘畅 on 2017/8/3.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
